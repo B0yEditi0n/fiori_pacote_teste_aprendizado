5d8aed7382598cfea4728a7e88c9b1fc05938d12
B0yEditi0n <ccaioabreudesouza9874@gmail.com>
2026-04-28 14:26:30 -0300

    exemplo de componente dinamico

--- imagens/webapp/controller/App.controller.js
+++ imagens/webapp/controller/App.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], (Controller, JSONModel) => {
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast",
+    "sap/m/Image",
+], (Controller, JSONModel, MessageToast, Image) => {
     "use strict";
 	return Controller.extend("dhconsulting.fiori.controller.App", {
         onInit(){
@@ -13,6 +15,28 @@ sap.ui.define([
 		    }), "img"); // Define o "img" como namespace para esse conjunto de dados
 
             // // this.getView().setModel(oImgModel, "img");
+
+            // Teste de Inclusão Dinamica
+            const oImage = new Image();
+            const oHbox = this.byId("hbox_content");
+            oImage.setSrc(sap.ui.require.toUrl("dhconsulting/fiori/assesents/dhlogoAlternativa.png"))
+            oHbox.addItem(oImage)
+        },
+
+        onPressImage(){
+            // Click 
+            MessageToast.show('Mensagem Muito Longa de imagem Pressionada')
+        },
+        onLoadImage(){
+            // Ao carregar imagem (Evento de inicialização)
+            MessageToast.show('Mensagem Muito Longa de imagem Carregada')
+        },
+        onErrorImage(){
+            // Caso a URL ou o conteudo não seja uma imagem.
+            MessageToast.show('Deu Ruim')
+        },
+        onTapImage(){
+            MessageToast.show('Tap image')
         }
         
 	});
