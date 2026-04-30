f1fab36334e15b8f7a3ec87a8f132a1eaacf4bd4
B0yEditi0n <ccaioabreudesouza9874@gmail.com>
2026-04-30 10:05:03 -0300

    ajustado exemplo inicial para comportar Typescript (é melhor pra dar manutenção em equipe)

--- app_em_branco/webapp/controller/App.controller.js
+++
@@ -1,7 +0,0 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-], (Controller, MessageToast) => {
-    "use strict";
-	return Controller.extend("dhconsulting.fiori.controller.App", {
-	});
-})
---
+++ app_em_branco/webapp/controller/App.controller.ts
@@ -0,0 +1,5 @@
+import Controller from "sap/ui/core/mvc/Controller";
+
+Controller.extend("dhconsulting.fiori.controller.App", {
+
+});
