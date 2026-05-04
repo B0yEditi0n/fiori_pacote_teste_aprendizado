7eb694c09d882cf323358520a01a9d72b911d26e
B0yEditi0n <ccaioabreudesouza9874@gmail.com>
2026-05-04 09:00:07 -0300

    adicionado dependencia que impede rotas de funcionar

--- app_em_branco/webapp/Component.ts
+++ app_em_branco/webapp/Component.ts
@@ -1,16 +1,16 @@
 import Control from "sap/ui/core/Control";
 import UIComponent from "sap/ui/core/UIComponent";
 import JSONModel from "sap/ui/model/json/JSONModel";
-
-const Component = UIComponent.extend("dhconsulting.fiori.Component", {
-    metadata: {
+/**
+ * @namespace dhconsulting.fiori
+ */
+export default class Component extends UIComponent {
+    public static metadata = {
+        manifest: "json",
         interfaces: ["sap.ui.core.IAsyncContentCreation"],
-        manifest: "json"
-    },
+    };
     
-    init() {
-        UIComponent.prototype.init.apply(this);
-    }
-});
-
-export default Component
+    public init(): void{
+        super.init();
+    };
+};
