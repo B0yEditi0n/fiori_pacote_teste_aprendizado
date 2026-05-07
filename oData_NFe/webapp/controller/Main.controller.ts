410fdab83c364c4027ab4a165d760d1054307c64
B0yEditi0n <ccaioabreudesouza9874@gmail.com>
2026-05-07 17:05:33 -0300

    atualização diaria

--- oData_NFe/webapp/controller/Main.controller.ts
+++ oData_NFe/webapp/controller/Main.controller.ts
@@ -1,13 +1,20 @@
 import Controller from "sap/ui/core/mvc/Controller";
-// import MessageToast from "sap/m/MessageToast";
+import MessageBox from "sap/m/MessageBox";
 // import Input from "sap/m/Input";
 import Model from "sap/ui/model/Model";
 import JSONModel from "sap/ui/model/json/JSONModel";
 import Event from "sap/ui/base/Event";
 import UIComponent from "sap/ui/core/UIComponent";
-import ODataModel from "sap/ui/model/odata/v2/ODataModel"
+import ODataModel from "sap/ui/model/odata/v4/ODataModel"
 
 import FilterBar from "sap/ui/mdc/FilterBar"
+import Filter from "sap/ui/model/Filter";
+import FilterOperator from "sap/ui/model/FilterOperator";
+
+// data get
+import ODataListBinding from "sap/ui/model/odata/v4/ODataListBinding";
+import ODataContextBinding from "sap/ui/model/odata/v4/ODataContextBinding";
+import ODataPropertyBinding from "sap/ui/model/odata/v4/ODataPropertyBinding";
 
 // import ODataModelV4 from "sap/ui/model/odata/v4/ODataModel"
 
@@ -22,7 +29,21 @@ export default class Main extends Controller{
         // UIComponent.getRouterFor(this).navTo("RouterTableResult")
         
     }
-    onBtnSearchPress(oEvent: Event){
+    runFilterPress(oEvent: Event){
+        const makeFilter = (
+            key : string, 
+            filterList: Array<object>
+        ) : String=>{
+            return ''
+        }
+
+        debugger
+
+        if((this.getView()!.byId("Main_FiltroID--filter--bukrs") as FilterBar).getConditions()){
+            MessageBox.error("Prencher valor da empresa")
+            return undefined;
+        };       
+
         const filters = this.getView()!.byId("Main_FiltroID") as FilterBar;
 
         const oConditions = (this.getView()!.byId("Main_FiltroID") as FilterBar).getFilterItems();
@@ -30,11 +51,18 @@ export default class Main extends Controller{
         const sParansCond = []
         for(let i = 0; i < oConditions.length; i++){            
             const oCond = oConditions[i];
-            sParansCond.push(JSON.stringify(oCond.getConditions(), "\t", 4));
-			
-            (this.getView()!.getModel()! as Model)
-            // .setProperty("/conditionsText", jsonConditions);
+            sParansCond.push(
+                makeFilter(oCond.getPropertyKey(), oCond.getConditions())
+            )
         }
-        debugger;
+        // new ODataContextBinding()
+
+        (this.getView()?.getModel("NFe_API") as ODataModel).bindList(
+            '/doc',
+            
+        )
+
+
     };
+
 };
